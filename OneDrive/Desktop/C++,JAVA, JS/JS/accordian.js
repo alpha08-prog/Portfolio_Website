4cdead48e18911567653ad1f1a3c2e454d176bd0
Atharva Agrawal <athrava08@gmail.com>
2026-03-11 14:21:18 +0530

    delete wrong files

--- OneDrive/Desktop/C++,JAVA, JS/JS/accordian.js
+++
@@ -1,7 +0,0 @@
-const accordian = document.getElementsByClassName("content-container");
-for(i=0; i<accordian.length;i++){
-    accordian[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-       
-    });
-}
--- OneDrive/Desktop/C++,JAVA, JS/LAB/jjf.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int* ptr = nullptr;
-    *ptr = 10;  
-
-    std::cout << "Value: " << *ptr << std::endl;
-    return 0;
-}
